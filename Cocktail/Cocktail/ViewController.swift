58dbceb92b177621d9f9090dd36336c7813297f4
Jhennyfer Rodrigues de Oliveira <jhennyfer@MacBook-Pro-de-Jhennyfer.local>
2020-08-12 11:13:46 -0300

    creating project

---
+++ Cocktail/Cocktail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktail
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 12/08/20.
+//  Copyright © 2020 Jhennyfer Rodrigues de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
