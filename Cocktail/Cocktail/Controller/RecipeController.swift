1ef5387e3ea252dce09845065852930969ca8337
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-08-15 01:45:07 -0300

    adding favorite button

--- Cocktail/Cocktail/Controller/RecipeController.swift
+++ Cocktail/Cocktail/Controller/RecipeController.swift
@@ -9,22 +9,60 @@
 import UIKit
 
 class RecipeController: UIViewController {
-
+    var isFavourited = false
+    @IBOutlet weak var glassTitle: UILabel!
+    @IBOutlet weak var glassText: UILabel!
+    @IBOutlet weak var ingredientTitle: UILabel!
+    @IBOutlet weak var directionTitle: UILabel!
+    @IBOutlet weak var ingredientText: UILabel!
+    @IBOutlet weak var directionText: UILabel!
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navBarApearence()
+        updateRighBarButton(isFavourite: isFavourited)
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - the heart shape of the button will change to fill
+    @objc func heartButtonTapped(button sender: UIBarButtonItem) {
+        self.isFavourited = !self.isFavourited
+        if self.isFavourited {
+            // save with file manager
+            self.favourite()
+        } else {
+            // delete with file manager
+            self.unfavourite()
+        }
+        self.updateRighBarButton(isFavourite: self.isFavourited)
     }
-    */
 
+}
+
+extension RecipeController {
+    private func navBarApearence() {
+        navigationItem.hidesBackButton = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = #colorLiteral(red: 1, green: 0.8289034963, blue: 0.4533876181, alpha: 1)
+        navigationItem.title = "Some Recipe"
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+    func updateRighBarButton(isFavourite: Bool) {
+        let heartButton = UIButton(type: .custom)
+        heartButton.addTarget(self, action: #selector(heartButtonTapped(button:)),
+                              for: .touchUpInside)
+        if isFavourite {
+            heartButton.setImage(UIImage(named: "heartRed"), for: .normal)
+        } else {
+            heartButton.setImage(UIImage(named: "heartEmpty"), for: .normal)
+        }
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: heartButton)
+    }
+    func favourite() {
+        //save with file manager
+    }
+    func unfavourite() {
+        //delete with file manager
+    }
 }
