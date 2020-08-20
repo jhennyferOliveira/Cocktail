0de35a524d4a571e3946df3c75b98e4c28cc36eb
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-08-20 10:44:19 -0300

    getting random cocktail from api and showing on the view

--- Cocktail/Cocktail/Controller/LuckCocktailController.swift
+++ Cocktail/Cocktail/Controller/LuckCocktailController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class LuckCocktailController: UIViewController {
-
     @IBOutlet weak var text: UILabel!
     @IBOutlet weak var image: UIImageView!
     let tapRec = UITapGestureRecognizer()
+    var recipe: Recipe?
+    var completeRecipe: RecipeM  = RecipeM()
+    var apiDataHandler = APIDataHandler()
     override func viewDidLoad() {
         super.viewDidLoad()
         tapRec.addTarget(self, action: #selector(tappedImage))
@@ -22,26 +24,53 @@ class LuckCocktailController: UIViewController {
         navBarAppearance.backgroundColor = #colorLiteral(red: 1, green: 0.8289034963, blue: 0.4533876181, alpha: 1)
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         navigationItem.title = "Luck Cocktail"
-
-        // Do any additional setup after loading the view.
     }
     @objc func tappedImage() {
+        getRandomCocktail()
         let storyboard = UIStoryboard(name: "RecipeView", bundle: nil)
-        let next = storyboard.instantiateViewController(withIdentifier: "RecipeView") as UIViewController
-        self.navigationController?.pushViewController(next, animated: true)
-        print("image tapped")
+        let next = storyboard.instantiateViewController(withIdentifier: "RecipeView") as? RecipeController
+        if let recipe = recipe {
+            completeRecipe = apiDataHandler.filterItemsFromRecipe(recipe: recipe)
+            let finalIngredients = apiDataHandler.mergeIngredientsAndMeasures(ingredients: completeRecipe.ingredients, measures: completeRecipe.measures)
+            next?.ingredientTextInit = finalIngredients
+            next?.glassTextInit = completeRecipe.glass
+            next?.drinkName = completeRecipe.title
+            next?.directionTextInit = completeRecipe.directions
+            let url = URL(string: completeRecipe.image)
+            if let url = url {
+                let data = try? Data(contentsOf: url)
+                if let data = data {
+                    if let myImage = UIImage(data: data) {
+                        next?.imageInit = myImage
+                    }
+                }
+            }
+        }
+        if let next = next {
+            self.navigationController?.pushViewController(next, animated: true)
+        }
     }
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.prefersLargeTitles = false
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension LuckCocktailController {
+    func getRandomCocktail() {
+        let path = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+        if let url: URL = URL(string: path) {
+            URLSession.shared.dataTask(with: url) { data, _, error in
+                if let data = data {
+                    do {
+                        let recipe = try JSONDecoder().decode(Recipe.self, from: data)
+                        DispatchQueue.main.async {
+                            self.recipe = recipe
+                        }
+                    } catch let error {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
     }
-    */
-
+    
 }
