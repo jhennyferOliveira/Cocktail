c1f156836384eb59dc907342ed7927ee1b351490
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-08-20 10:39:50 -0300

    creating new files

---
+++ Cocktail/Cocktail/Model/DrinkM.swift
@@ -0,0 +1,9 @@
+//
+//  DrinkM.swift
+//  Cocktail
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 17/08/20.
+//  Copyright © 2020 Jhennyfer Rodrigues de Oliveira. All rights reserved.
+//
+
+import Foundation
