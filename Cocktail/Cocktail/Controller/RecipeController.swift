7ee4f96843551003e54a285eaa5953c79cabd673
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-08-24 00:14:44 -0300

    adding api call methods

--- Cocktail/Cocktail/Controller/RecipeController.swift
+++ Cocktail/Cocktail/Controller/RecipeController.swift
@@ -9,29 +9,42 @@
 import UIKit
 
 class RecipeController: UIViewController {
-    var isFavourited = false
     @IBOutlet weak var glassTitle: UILabel!
-    @IBOutlet weak var glassText: UILabel!
+    @IBOutlet weak var glassText: UILabel! 
     @IBOutlet weak var ingredientTitle: UILabel!
     @IBOutlet weak var directionTitle: UILabel!
     @IBOutlet weak var ingredientText: UILabel!
     @IBOutlet weak var directionText: UILabel!
     @IBOutlet weak var image: UIImageView!
-    var glassTextInit = ""
-    var ingredientTextInit = ""
-    var directionTextInit = ""
-    var imageInit = #imageLiteral(resourceName: "Cocktail")
-    var drinkName = ""
+    enum PreviousView {
+        case luckCocktail
+        case tableView
+        case favorites
+    }
+    var previous = PreviousView.luckCocktail
+    var savedRecipe: Recipe?
+    var isFavourited = false
+    var drinkId = ""
+    var drinkUUID = UUID()
+    var apiDataHandler = APIDataHandler()
+    var completeRecipe = RecipeM()
+    let fileHelper = FileHelper()
+    var imageHelper = ImageLoader()
+    private var recipeRepository: RecipeRepository {
+        RecipeRepository()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         navBarApearence()
-        navigationItem.title = drinkName
         updateRighBarButton(isFavourite: isFavourited)
-        glassText.text = glassTextInit
-        ingredientText.text = ingredientTextInit
-        directionText.text = directionTextInit
-        image.image = imageInit
-        // Do any additional setup after loading the view.
+        switch previous {
+        case .favorites:
+            getFavoriteRecipe()
+        case .luckCocktail:
+            getRandomCocktail()
+        case .tableView:
+            getDrink(drinkId: drinkId)
+        }
     }
     // MARK: - the heart shape of the button will change to fill
     @objc func heartButtonTapped(button sender: UIBarButtonItem) {
@@ -45,9 +58,7 @@ class RecipeController: UIViewController {
         }
         self.updateRighBarButton(isFavourite: self.isFavourited)
     }
-
 }
-
 extension RecipeController {
     private func navBarApearence() {
         navigationItem.hidesBackButton = false
@@ -69,10 +80,88 @@ extension RecipeController {
         }
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: heartButton)
     }
+    func updateView(recipe: Recipe) {
+        let recipes = recipeRepository.readAllItems()
+        completeRecipe = apiDataHandler.filterItemsFromRecipe(recipe: recipe)
+        let ingredients = apiDataHandler
+            .mergeIngredientsAndMeasures(ingredients: completeRecipe.ingredients,
+                                         measures: completeRecipe.measures)
+        navigationItem.title = completeRecipe.title
+        glassText.text = completeRecipe.glass
+        ingredientText.text = ingredients
+        directionText.text = completeRecipe.directions
+        imageHelper.obtainImageWithPath(imagePath: completeRecipe.image) { (image) in
+            self.image.image = image
+        }
+        isFavourited =  recipes.contains(where: { (recipe) -> Bool in
+            if completeRecipe.title == recipe.title {
+                return true
+            } else {
+                return false
+            }
+        })
+    }
     func favourite() {
         //save with file manager
+        let newRecipe = recipeRepository.createNewItem()
+        newRecipe?.glass = completeRecipe.glass
+        newRecipe?.image = completeRecipe.image
+        newRecipe?.ingredients = completeRecipe.ingredients
+        newRecipe?.measures = completeRecipe.measures
+        newRecipe?.title = completeRecipe.title
+        newRecipe?.directions = completeRecipe.directions
+        recipeRepository.update(item: newRecipe ?? RecipeM())
     }
     func unfavourite() {
         //delete with file manager
+        recipeRepository.delete(id: completeRecipe.identifier)
+    }
+    private func getDrink(drinkId: String) {
+        let path = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId
+        if let url: URL = URL(string: path) {
+            URLSession.shared.dataTask(with: url) { data, _, error in
+                if let data = data {
+                    do {
+                        let recipe = try JSONDecoder().decode(Recipe.self, from: data)
+                        DispatchQueue.main.async {
+                            self.updateView(recipe: recipe)
+                        }
+                    } catch let error {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+    }
+    func getRandomCocktail() {
+        let path = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+        if let url: URL = URL(string: path) {
+            URLSession.shared.dataTask(with: url) { data, _, error in
+                if let data = data {
+                    do {
+                        let recipe = try JSONDecoder().decode(Recipe.self, from: data)
+                        DispatchQueue.main.async {
+                            self.updateView(recipe: recipe)
+                        }
+                    } catch let error {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+    }
+    func getFavoriteRecipe() {
+        guard let recipe = recipeRepository.readItem(id: drinkUUID) else { return }
+        let ingredients = apiDataHandler
+            .mergeIngredientsAndMeasures(ingredients: recipe.ingredients,
+                                         measures: recipe.measures)
+        navigationItem.title = recipe.title
+        glassText.text = recipe.glass
+        ingredientText.text = ingredients
+        directionText.text = recipe.directions
+        imageHelper.obtainImageWithPath(imagePath: recipe.image) { (image) in
+            self.image.image = image
+        }
+        isFavourited = true
     }
 }
