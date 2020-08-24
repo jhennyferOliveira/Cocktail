1f915e5ef79c3b05c250cd899a488fcb5772f853
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-08-24 00:07:55 -0300

    dividing responsabilities this controller contains only the necessary

--- Cocktail/Cocktail/Controller/LuckCocktailController.swift
+++ Cocktail/Cocktail/Controller/LuckCocktailController.swift
@@ -15,6 +15,9 @@ class LuckCocktailController: UIViewController {
     var recipe: Recipe?
     var completeRecipe: RecipeM  = RecipeM()
     var apiDataHandler = APIDataHandler()
+    private var recipeRepository: RecipeRepository {
+        RecipeRepository()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         tapRec.addTarget(self, action: #selector(tappedImage))
@@ -26,26 +29,9 @@ class LuckCocktailController: UIViewController {
         navigationItem.title = "Luck Cocktail"
     }
     @objc func tappedImage() {
-        getRandomCocktail()
         let storyboard = UIStoryboard(name: "RecipeView", bundle: nil)
         let next = storyboard.instantiateViewController(withIdentifier: "RecipeView") as? RecipeController
-        if let recipe = recipe {
-            completeRecipe = apiDataHandler.filterItemsFromRecipe(recipe: recipe)
-            let finalIngredients = apiDataHandler.mergeIngredientsAndMeasures(ingredients: completeRecipe.ingredients, measures: completeRecipe.measures)
-            next?.ingredientTextInit = finalIngredients
-            next?.glassTextInit = completeRecipe.glass
-            next?.drinkName = completeRecipe.title
-            next?.directionTextInit = completeRecipe.directions
-            let url = URL(string: completeRecipe.image)
-            if let url = url {
-                let data = try? Data(contentsOf: url)
-                if let data = data {
-                    if let myImage = UIImage(data: data) {
-                        next?.imageInit = myImage
-                    }
-                }
-            }
-        }
+        next?.previous = .luckCocktail
         if let next = next {
             self.navigationController?.pushViewController(next, animated: true)
         }
@@ -53,24 +39,4 @@ class LuckCocktailController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.prefersLargeTitles = false
     }
-}
-extension LuckCocktailController {
-    func getRandomCocktail() {
-        let path = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-        if let url: URL = URL(string: path) {
-            URLSession.shared.dataTask(with: url) { data, _, error in
-                if let data = data {
-                    do {
-                        let recipe = try JSONDecoder().decode(Recipe.self, from: data)
-                        DispatchQueue.main.async {
-                            self.recipe = recipe
-                        }
-                    } catch let error {
-                        print(error)
-                    }
-                }
-            }.resume()
-        }
-    }
-    
 }
